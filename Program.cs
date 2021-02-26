96c2d97aab7c24f05f93a1e5c2e62411b1577b1a
Andrea <chela355@hotmail.com>
2021-02-26 17:46:39 -0500

    Se realizo el trabajo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Conversion_de_Unidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Ingrese el valor que quiere convertir: ");
+             double valoraConvertir= Convert.ToInt32(Console.ReadLine());
+             double pulgada= valoraConvertir / 2.5;
+
+            Console.WriteLine("El resultado es " + pulgada.ToString());
+        }
+    }
+}
